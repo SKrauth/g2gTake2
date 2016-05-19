e3e288e15eda62c87ce43fb5af3a039a10a528f6
SKrauth <spkrauth@gmail.com>
2016-05-18 21:55:18 -0400

    bootstrapped and pages setup in server

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ server.get("/", function(req, res){
   res.sendFile("public/index.html");
 });
 
+server.get("/focus", function(req, res){
+  res.sendFile(__dirname+"/public/html/pages/focus.html")
+});
+
+server.get("/list", function(req, res){
+  res.sendFile(__dirname+"/public/html/pages/list.html")
+});
+
+server.get("/form", function(req, res){
+  res.sendFile(__dirname+"/public/html/pages/ratingForm.html")
+});
+
+
 server.listen(8080, function(){
   console.log("Server now listening on port 8080");
 });
