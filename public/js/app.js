23a3198dd38431e48281bd8fd2e15065e82bae17
SKrauth <spkrauth@gmail.com>
2016-06-07 19:24:39 -0400

    adjusted relative file routes to work better with sfc server

--- public/js/app.js
+++ public/js/app.js
@@ -3,35 +3,35 @@ var app = angular.module('app', ['ngRoute']);
 app.controller('gotToGo', function($scope) {
   // Lots of code to control navigation.
   $scope.header = function(){
-    return "../html/layouts/_header.html";
+    return "html/layouts/_header.html";
   };
 
   $scope.home = function(){
-    return "../html/pages/_home.html";
+    return "html/pages/_home.html";
   };
 
   $scope.focus = function(){
-    return "../html/pages/_focus.html";
+    return "html/pages/_focus.html";
   };
 
   $scope.list = function(){
-    return "../html/pages/_list.html";
+    return "html/pages/_list.html";
   };
 
   $scope.ratingForm = function(){
-    return "../html/pages/_ratingForm.html";
+    return "html/pages/_ratingForm.html";
   };
 
   $scope.filter = function(){
-    return "../html/pages/_filter.html";
+    return "html/pages/_filter.html";
   };
 
   $scope.about = function(){
-    return "../html/pages/_about.html";
+    return "html/pages/_about.html";
   };
 
   $scope.footer = function(){
-    return "../html/layouts/_footer.html";
+    return "html/layouts/_footer.html";
   };
 
   // creates and manages currentPage variable to control switch statement based nav
