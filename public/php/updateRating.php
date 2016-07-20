3f4b799a97a65e08d0d5b018b917af1db5881258
SKrauth <spkrauth@gmail.com>
2016-07-20 16:41:40 -0400

    troubleshooting update

--- public/php/updateRating.php
+++ public/php/updateRating.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     require_once 'connect.php';
     if(isset($_GET['ratingID'])){
         //this pile of nonsense came about because I was having trouble getting angular to pass objects or arrays correctly, so I had to hard code each variable. It's aweful, hopefully there will be a better solution soon.
@@ -11,13 +11,13 @@
         $address = $_GET['address'];
         $description = $_GET['description'];
 
-        $query="UPDATE ratings set name='$name', sanitation='$sanitation', toiletries='$toiletries', overall='$overall', 
+        $query="UPDATE ratings set name='$name', sanitation='$sanitation', toiletries='$toiletries', overall='$overall',
             gender='$gender', address='$address', description='$description' where id='$ratingID'";
-        
+
         $result = $connection->query($query) or die($connection->error.__LINE__);
- 
+
         $result = $connection->affected_rows;
- 
+
         $json_response = json_encode($result);
     };
 ?>
